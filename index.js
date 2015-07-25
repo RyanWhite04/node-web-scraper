4ae5dec64de689727dec2b4fc1276b5fd4a47810
Ryan White <ryanwhite04@gmail.com>
2015-07-25 19:22:18 +0800

    saves html files now

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
 var fs = require('fs');
 var request = require('request');
-var cheerio = require('cheerio');
 var mkdirp = require('mkdirp');
+var cheerio = require('cheerio');
 
-href = 'http://readinglists.ucl.ac.uk/index.html?browse';
-// href = 'http://readinglists.ucl.ac.uk/faculties/art.html';
+// href = 'http://readinglists.ucl.ac.uk/index.html?browse';
+href = 'http://readinglists.ucl.ac.uk/faculties/art.html';
 // href = 'http://readinglists.ucl.ac.uk/departments/basco_art.html';
 // href = 'http://readinglists.ucl.ac.uk/modules/basc1001.html';
 
-mkdirp('json', function() {
-    scrape(href);
-});
+getHTML(href);
 
-function scrape(href) {
-    request(href, function(error, response, html){
-        var $ = cheerio.load(html);
-        var nodes = $('tr[data-node-uri]>td>a.nodeName');
-        var file = href.split('.html')[0].split('http://readinglists.ucl.ac.uk/')[1];
-        var dirp = file.split('/');
-        var last = dirp.pop();
-        dirp.join('/');
-        mkdirp('json/' + dirp, function() {
-            if ($('tr[data-node-uri]>td>a.nodeName').length) getList(file, $);
-            else getUnit(file, $);
-        });
-        console.log('dirp: ' + dirp, 'href: ' + href);
+function getHTML(href, callback) {
+
+    var file = 'html/' + href.split('.html')[0].split('http://readinglists.ucl.ac.uk/')[1] + '.html';
+    fs.readFile(file, 'utf8', function (err, html) {
+        console.log('file: ' + file);
+        if (err) {
+            console.log('err: ' + err);
+            if (err.errno === -2) {
+                console.log('404');
+                request(href, function(err, response, html) {
+                    if (err) console.log('err: ' + err);
+                    saveHTML(href, html, function() {
+                        console.log('html file saved');
+                        scrape(html);
+                    });
+                });
+            }
+        }
+        else {
+            console.log('file found');
+            scrape(html);
+        }
     });
+
+}
+
+function scrape(html) {
+    var $ = cheerio.load(html);
+    var nodes = $('tr[data-node-uri]>td>a.nodeName');
+    var file = href.split('.html')[0].split('http://readinglists.ucl.ac.uk/')[1];
+    var dirp = file.split('/');
+    var last = dirp.pop();
+    dirp.join('/');
+    mkdirp('json/' + dirp, function() {
+        if ($('tr[data-node-uri]>td>a.nodeName').length) getList(file, $);
+        else getUnit(file, $);
+    });
+    console.log('dirp: ' + dirp, 'href: ' + href);
 }
 
 function getList(file, $) {
@@ -40,7 +62,7 @@ function getList(file, $) {
             'name': name,
             'file': file
         });
-        scrape(href);
+        getHTML(href);
     });
     fs.writeFile('json/' + file + '.json', JSON.stringify(json, null, 4), function(err){
         if (err) console.log(err);
@@ -66,3 +88,40 @@ function getUnit(file, $) {
         });
     }
 }
+
+function saveHTML(href, html, callback) {
+    console.log('saveHTML', 'href: ' + href);
+    var file = href.split('.html')[0].split('http://readinglists.ucl.ac.uk/')[1];
+    var dirp = file.split('/');
+    var last = dirp.pop();
+    dirp.join('/');
+    mkdirp('html/' + dirp, function() {
+        console.log('dirp: ' + dirp, 'href: ' + href);
+        fs.writeFile('html/' + file + '.html', html, function(err){
+            if (err) console.log(err);
+            callback();
+            // console.log('File successfully written');
+        });
+    });
+}
+
+function saveJSON(href, json, callback) {
+    var file = href.split('.html')[0].split('http://readinglists.ucl.ac.uk/')[1];
+    var dirp = file.split('/');
+    var last = dirp.pop();
+    dirp.join('/');
+    mkdirp('json/' + dirp, function() {
+        console.log('dirp: ' + dirp, 'href: ' + href);
+        fs.writeFile('json/' + path + '.json', JSON.stringify(json, null, 4), function(err){
+            if (err) console.log(err);
+            callback();
+            // console.log('File successfully written');
+        });
+    });
+}
+
+
+// fs.readFile('not.there', function(err, data) {
+//     if (err) throw err;
+//     console.log(data);
+// });
